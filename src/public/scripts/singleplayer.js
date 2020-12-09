f9d02ca266c918e476eaf013fef61478c4bef493
Matheus Leon <mlan@cin.ufpe.br>
2020-12-09 04:56:40 -0300

    feat: render pokemon moves

--- src/public/scripts/singleplayer.js
+++ src/public/scripts/singleplayer.js
@@ -1,16 +1,16 @@
 function clearContainers() {
-    let pokemon_menu = document.querySelector('#pokemon-menu');
-    pokemon_menu = "";
-    let pokemon_container = document.querySelector('#pokemon-container');
-    pokemon_container = "";
+    var pokemon_menu = document.querySelector('#pokemon-menu');
+    pokemon_menu.innerHTML = "";
+    var pokemon_container = document.querySelector('#pokemon-container');
+    //pokemon_container.innerHTML = "";
 }
 
-function capitalizeFirstLetter(string) {
+function capitalizeFirstvarter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function renderPokemonDropdownLists() {
-    clearContainers();
+    //clearContainers();
 
     fetchKantoPokemon().then(allPokemons => {  
         renderPokemonDropdownList(allPokemons.results, 'pokemon_names_dropdown1');
@@ -19,21 +19,20 @@ function renderPokemonDropdownLists() {
 } 
 
 function renderPokemonDropdownList(allPokemons, dropdownlist_name) {
-    let dropdown = document.getElementById(dropdownlist_name);
+    var dropdown = document.getElementById(dropdownlist_name);
     dropdown.length = 0;
 
-    let defaultOption = document.createElement('option');
+    var defaultOption = document.createElement('option');
     defaultOption.text = 'Choose your Pokemon';
     dropdown.add(defaultOption);
     dropdown.selectedIndex = 0;
 
     for (var i = 0; i < allPokemons.length; i++) {
-        let option = document.createElement('option');
-        option.text = capitalizeFirstLetter(allPokemons[i].name);
+        var option = document.createElement('option');
+        option.text = capitalizeFirstvarter(allPokemons[i].name);
         option.value = allPokemons[i].name;
         dropdown.add(option);
     }
-
 }
 
 function fetchKantoPokemon() {
@@ -46,29 +45,66 @@ function fetchKantoPokemon() {
 
 function renderPokemonImage(pokemon_name, container_div) {
     pokemon_info_url = 'https://pokeapi.co/api/v2/pokemon/' + pokemon_name;
-    let poke_img_container = document.createElement('div')
+    var poke_img_container = document.createElement('div')
     poke_img_container.classList.add('image')
 
     fetch(pokemon_info_url)
     .then(results => results.json())
     .then(pokemon_data => {
-        // console.log(pokemon_data.moves[0])
-        // console.log(pokemon_data.id);
-        let poke_img = document.createElement('img')
+        var poke_img = document.createElement('img')
         poke_img.srcset = `https://pokeres.bastionbot.org/images/pokemon/${pokemon_data.id}.png`
-        
-        poke_img_container.append(poke_img, [pokemon_data.moves[0].move.name, pokemon_data.moves[1].move.name]);
+        poke_img_container.append(poke_img)
+
         container_div.append(poke_img_container);
     })
 }
 
+async function getPokemonMoves(pokemon_name) {
+    pokemon_info_url = 'https://pokeapi.co/api/v2/pokemon/' + pokemon_name;
+    var moves = []
+    var counter = 0;
 
-function renderPokemon(pokemon_name, all_pokemon_container) {
-    let poke_container = document.createElement("div")
-    poke_container.classList.add('ui', 'card');
+    await fetch(pokemon_info_url)
+    .then(results => results.json())
+    .then(pokemon_data => {
+        pokemon_data.moves.forEach(element => {
+            //console.log(element)
+            if (moves.length < 4 && element.version_group_details[0].move_learn_method.name == 'level-up') {
+                moves.push(element.move.name)
+                //console.log('aq ' + [element.move.name, element.move.url])
+                counter++;
+            }
+        });
+    })
+    return moves;
+}
+
+function renderPokemonMoves(pokemon_name, container_div, player_id) {
+    pokemon_info_url = 'https://pokeapi.co/api/v2/pokemon/' + pokemon_name;
+    
+    getPokemonMoves(pokemon_name).then(moves => {
+        counter = 0
+        for (var i = 0; i < 4; i++) {
+            var button_name = `player${player_id}-move${i}`;
+            var poke_move = document.getElementById(button_name)
+            poke_move.innerHTML = moves[i];
+
+            // var poke_move = document.createElement("BUTTON");
+            // poke_move.innerHTML = move;
+            // container_div.append(poke_move);
+
+        }
+    });
+}
+
+
+function renderPokemon(pokemon_name, all_pokemon_container, player_id) {
+    var poke_container = document.createElement("div")
+    //poke_container.classList.add('ui', 'card');
     renderPokemonImage(pokemon_name, poke_container);
+    renderPokemonMoves(pokemon_name, all_pokemon_container, player_id);
 
-    let pokeName = document.createElement('h4') 
+    var pokeName = document.createElement('h4') 
     pokeName.innerText = pokemon_name
 
     poke_container.append(pokeName);
@@ -76,15 +112,14 @@ function renderPokemon(pokemon_name, all_pokemon_container) {
 }
 
 function playGame() {
-    clearContainers();
-    
     var pokemon1 = document.getElementById('pokemon_names_dropdown1').value
     var pokemon2 = document.getElementById('pokemon_names_dropdown2').value
+    //var pokemon1 = 'charmander';
+    //var pokemon2 = 'squirtle';
 
-    // console.log(pokemon1 + ' e ' + pokemon2);
+    var all_pokemon_container = document.querySelector('#pokemon-container');
 
-    let all_pokemon_container = document.querySelector('#pokemon-container');
-
-    renderPokemon(pokemon1, all_pokemon_container);
-    renderPokemon(pokemon2, all_pokemon_container);
+    renderPokemon(pokemon1, all_pokemon_container, 0);
+    renderPokemon(pokemon2, all_pokemon_container, 0);
+    clearContainers();
 }
