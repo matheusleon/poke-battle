8cb22d2e49460c1eea304078211975da3b97ccaf
Matheus Leon <mlan@cin.ufpe.br>
2020-12-09 09:08:00 -0300

    feat: add pokemon health and moves damage

--- src/public/scripts/singleplayer.js
+++ src/public/scripts/singleplayer.js
@@ -1,3 +1,9 @@
+
+var moves = new Array(2);
+var health = new Array(2);
+var max_health = new Array(2);
+var pokemon = new Array(2);
+
 function clearMenu() {
     var pokemon_menu = document.querySelector('#pokemon-menu');
     pokemon_menu.innerHTML = "";
@@ -6,7 +12,8 @@ function clearMenu() {
 // TODO: Create functions to change visibility of menu and game
 // fuction changeVisibility() {}
 
-function capitalizeFirstvarter(string) {
+function capitalizeFirstLetter(string) {
+    console.log(string)
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
@@ -28,7 +35,7 @@ function renderPokemonDropdownList(allPokemons, dropdownlist_name) {
 
     for (var i = 0; i < allPokemons.length; i++) {
         var option = document.createElement('option');
-        option.text = capitalizeFirstvarter(allPokemons[i].name);
+        option.text = capitalizeFirstLetter(allPokemons[i].name);
         option.value = allPokemons[i].name;
         dropdown.add(option);
     }
@@ -50,19 +57,23 @@ function renderPokemonImage(pokemon_name, player_id) {
     .then(pokemon_data => {
         var poke_img = document.getElementById(`pokemon${player_id}-img`)
         poke_img.srcset = `https://pokeres.bastionbot.org/images/pokemon/${pokemon_data.id}.png`
+        poke_img.style.width = '400px';
+        poke_img.style.height = 'auto';
     })
 }
 
-async function getPokemonMoves(pokemon_name) {
+async function getPokemonMoves(pokemon_name, player_id) {
     pokemon_info_url = 'https://pokeapi.co/api/v2/pokemon/' + pokemon_name;
-    var moves = []
 
     await fetch(pokemon_info_url)
     .then(results => results.json())
     .then(pokemon_data => {
+        var counter = 0;
         pokemon_data.moves.forEach(element => {
-            if (moves.length < 4 && element.version_group_details[0].move_learn_method.name == 'level-up') {
-                moves.push(element.move.name)
+            if (counter < 4 && element.version_group_details[0].move_learn_method.name == 'level-up') {
+                moves[player_id][counter] = [element.move.name, element.move.url];
+                console.log([element.move.name, element.move.url])
+                counter++;
             }
         });
     })
@@ -71,21 +82,39 @@ async function getPokemonMoves(pokemon_name) {
 
 function renderPokemonMoves(pokemon_name, player_id) {
     pokemon_info_url = 'https://pokeapi.co/api/v2/pokemon/' + pokemon_name;
+    moves[0] = new Array(4);
+    moves[1] = new Array(4);
     
-    getPokemonMoves(pokemon_name).then(moves => {
+    getPokemonMoves(pokemon_name, player_id).then(moves => {
         counter = 0
         for (var i = 0; i < 4; i++) {
             var button_name = `pokemon${player_id}-move${i}`;
-            var poke_move = document.getElementById(button_name)
-            poke_move.innerHTML = moves[i];
+            var poke_move = document.getElementById(button_name);
+            console.log(moves[player_id][i]);
+            poke_move.innerHTML = moves[player_id][i][0];
             poke_move.style.visibility = "visible";
         }
     });
 }
 
 function renderPokemonName(pokemon_name, player_id) {
-    var pokeName = document.getElementById(`pokemon${player_id}-name`) 
-    pokeName.innerText = pokemon_name
+    var pokeName = document.getElementById(`pokemon${player_id}-name`);
+    pokeName.innerText = capitalizeFirstLetter(pokemon_name);
+}
+
+async function renderPokemonHealth(pokemon_name, player_id) {
+    pokemon_info_url = 'https://pokeapi.co/api/v2/pokemon/' + pokemon_name;
+
+    await fetch(pokemon_info_url)
+    .then(response => response.json())
+    .then(pokemon_data => {
+        console.log('health = ' + pokemon_data.stats[0].base_stat);
+        health[player_id] = pokemon_data.stats[0].base_stat;
+        max_health[player_id] = pokemon_data.stats[0].base_stat;
+    });
+
+    var pokeHealth = document.getElementById(`pokemon${player_id}-health`);
+    pokeHealth.innerText = `${health[player_id]} / ${max_health[player_id]}`;
 }
 
 function renderPokemon(pokemon_name, player_id) {
@@ -94,20 +123,71 @@ function renderPokemon(pokemon_name, player_id) {
     renderPokemonImage(pokemon_name, player_id);
     renderPokemonMoves(pokemon_name, player_id);
     renderPokemonName(pokemon_name, player_id);
+    renderPokemonHealth(pokemon_name, player_id);
+}
+
+function updatePokemonHealth(player_id) {
+    var pokeHealth = document.getElementById(`pokemon${player_id}-health`);
+    pokeHealth.innerText = `${health[player_id]} / ${max_health[player_id]}`;
+}
+
+async function player0move0() {
+    var damage = 0
+    var message = ''
+
+    // Fetch move from Api. Ex.: https://pokeapi.co/api/v2/move/52/
+    await fetch(moves[0][0][1])
+    .then(response => response.json())
+    .then(move_data => {
+        console.log('power = ' + move_data.power)
+        damage = move_data.power
+        message = move_data.flavor_text_entries[0].flavor_text
+        health[1] = Math.max(0, health[1] - damage)
+        updatePokemonHealth(1)
+    })
+    var moves_container = document.getElementById('moves-text');
+    //moves_container.innerHTML += `Jogador 1 causou ${damage} de dano<br>`;
+    moves_container.innerHTML += pokemon[0] + ' uses ' + moves[0][0][0] + `!<br>`;
+    moves_container.innerHTML += message + `<br>`;
+
+    if (health[1] <= 0) {
+        endGame();
+    }
+}
+
+function disableButtons() {
+    for (var i = 0; i < 2; i++) {
+        for (var j = 0; j < 4; j++) {
+            var button_name = `pokemon${i}-move${j}`;
+            document.getElementById(button_name).disabled = true;
+        }
+    }
+}
+
+function endGame() {
+    disableButtons();
+
+    if (health[0] <= 0) {
+        var moves_container = document.getElementById('moves-text');
+        moves_container.innerHTML += `Jogador 2 venceu <br>`;
+    } else {
+        var moves_container = document.getElementById('moves-text');
+        moves_container.innerHTML += `Jogador 1 venceu <br>`;
+    }
 }
 
 function startSingleplayer() {
-    //renderPokemonDropdownLists(); 
-    playGame();
+    renderPokemonDropdownLists(); 
+    //playGame();
 }
 
 function playGame() {
-    //var pokemon1 = document.getElementById('pokemon_names_dropdown1').value
-    //var pokemon2 = document.getElementById('pokemon_names_dropdown2').value
-    var pokemon1 = 'charmander';
-    var pokemon2 = 'squirtle';
+    pokemon[0] = document.getElementById('pokemon_names_dropdown1').value
+    pokemon[1] = document.getElementById('pokemon_names_dropdown2').value
+    //pokemon[0] = 'charmander';
+    //pokemon[1] = 'squirtle';
 
-    renderPokemon(pokemon1, 0);
-    renderPokemon(pokemon2, 1);
+    renderPokemon(pokemon[0], 0);
+    renderPokemon(pokemon[1], 1);
     clearMenu();
 }
